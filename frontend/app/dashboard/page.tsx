e022e2943401519800a7067f7f1c57e9753df371
Manav <manav18gadhiya@gmail.com>
2025-02-22 21:48:03 +0530

    add: Google OAuth

---
+++ frontend/app/dashboard/page.tsx
@@ -0,0 +1,20 @@
+// THIS IS A TEST PAGE FOR AUTH
+
+"use client";
+
+import { useSession } from "next-auth/react";
+import LogoutButton from "@/components/LogoutButton";
+
+export default function Dashboard() {
+  const { data: session } = useSession();
+
+  if (!session) return <p>Redirecting...</p>;
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl">Dashboard</h1>
+      <p>Welcome, {session.user?.name}!</p>
+      <LogoutButton />
+    </div>
+  );
+}
